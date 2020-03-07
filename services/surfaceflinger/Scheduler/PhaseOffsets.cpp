shin32125/android_frameworks_native

Fix 44237b48c6849d7bdbcb1a5e9558d43d44015081

services/surfaceflinger/Scheduler/PhaseOffsets.cpp
@@ -97,12 +97,12 @@ PhaseOffsets::PhaseOffsets() {
                            highFpsLateAppOffsetNs};
 
     mOffsets.insert({RefreshRateType::POWER_SAVING, defaultOffsets});
-    mOffsets.insert({RefreshRateType::LOW0, defaultOffsets});
-    mOffsets.insert({RefreshRateType::LOW1, defaultOffsets});
-    mOffsets.insert({RefreshRateType::LOW2, defaultOffsets});
     mOffsets.insert({RefreshRateType::DEFAULT, defaultOffsets});
     mOffsets.insert({RefreshRateType::PERFORMANCE, highFpsOffsets});
 #ifdef QCOM_UM_FAMILY
+    mOffsets.insert({RefreshRateType::LOW0, defaultOffsets});
+    mOffsets.insert({RefreshRateType::LOW1, defaultOffsets});
+    mOffsets.insert({RefreshRateType::LOW2, defaultOffsets});
     mOffsets.insert({RefreshRateType::PERF1, highFpsOffsets});
     mOffsets.insert({RefreshRateType::PERF2, highFpsOffsets});
 #endif

IPSBHANGU/native

services/surfaceflinger/Scheduler/PhaseOffsets.cpp
@@ -140,9 +140,7 @@ PhaseOffsets::PhaseOffsets() {
     mOffsets.insert({RefreshRateType::LOW1, defaultOffsets});
     mOffsets.insert({RefreshRateType::LOW2, defaultOffsets});
     mOffsets.insert({RefreshRateType::DEFAULT, defaultOffsets});
-    mOffsets.insert({RefreshRateType::PERFORMANCE, perfFpsOffsets});
-    mOffsets.insert({RefreshRateType::HIGH1, highFpsOffsets});
-    mOffsets.insert({RefreshRateType::HIGH2, highFpsOffsets});
+    mOffsets.insert({RefreshRateType::PERFORMANCE, highFpsOffsets});
 
     mOffsetThresholdForNextVsync = phaseOffsetThresholdForNextVsyncNs != -1
             ? phaseOffsetThresholdForNextVsyncNs